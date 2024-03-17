d37f783055f9d42ec14f99512a985a0e5192c398
Skywalker <imsho0218@gmail.com>
2024-03-17 23:40:47 +0800

    update

--- python/787_Cheapest_Flights_Within_K_Stops.py
+++ python/787_Cheapest_Flights_Within_K_Stops.py
@@ -1,56 +1,18 @@
 from typing import List
 
 class Solution:
-
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        start = []
-        for i in range(len(flights)):
-            if flights[i][0] == src:
-                start.append(flights[i])
+        dp = [float('inf')] * n
+        dp[src] = 0
 
-
-        prob = []
-        for i in range(len(start)):
-            spot = k
-            total = 0
-            end = False
-            stop_p = start[i][1]
-            start_p = start[i][0]
-            # total += start[i][2]
-            while spot >= 0:
-                for i in range(len(flights)):
-                    if start_p == flights[i][0] and stop_p == flights[i][1]:
-                        total += flights[i][2]
-                        spot -= 1
-                        flights.remove(flights[i])
-                        if stop_p == dst:
-                            end = True
-                            break
-                        else:
-                            start_p = stop_p
-                            # start.append(flights[i])
-
-                    elif start_p == flights[i][0]:
-                        stop_p = flights[i][1]
-                        total += flights[i][2]
-                        spot -= 1
-                        flights.remove(flights[i])
-                        if stop_p == dst:
-                            end = True
-                            break
-                        else:
-                            start_p = stop_p
-                            # start.append(flights[i])
-
-                # for i in range(len(flights)):
-                #     if stop_p == flights[i][0]:
-                #         start_p = stop_p
-                #         stop_p = flights[i][1]
-            if end == True:
-                prob.append(total)
-
-        print(start)
-        print(prob)
+        for _ in range(k + 1):
+            temp = dp[:]
+            for flight in flights:
+                if dp[flight[0]] != float('inf'):
+                    temp[flight[1]] = min(temp[flight[1]], dp[flight[0]] + flight[2])
+            dp = temp
+        
+        return dp[dst] if dp[dst] != float('inf') else -1
 
 s = Solution()
 ans = s.findCheapestPrice(3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 1)
